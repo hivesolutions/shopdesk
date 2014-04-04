506e72413fa1ba150e5eec03abc2723071c833d6
joamag <joamag@gmail.com>
2014-04-04 16:26:36 +0100

    new binaries

--- src/shopdesk/static/js/main.js
+++ src/shopdesk/static/js/main.js
@@ -1,2 +1,21 @@
 jQuery(document).ready(function() {
+            jQuery(".side-links .hide").click(function() {
+                        var element = jQuery(this);
+                        var sideLinks = element.parents(".side-links");
+                        sideLinks.triggerHandler("hide");
+                    });
+
+            jQuery(".side-links .swindow").click(function() {
+                        alert("ola");
+                    });
+
+
+            jQuery(".side-links .side").click(function() {
+                        jQuery(".side-panel").triggerHandler("toggle");
+                    });
+
+            jQuery(".logo").click(function() {
+                        var sideLinks = jQuery(".side-links");
+                        sideLinks.triggerHandler("toggle");
+                    });
         });
