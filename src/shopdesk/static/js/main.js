c23fe26302caa2ccc96976ba7dfd697bc571be24
joamag <joamag@gmail.com>
2014-04-05 15:10:42 +0100

    new code

--- src/shopdesk/static/js/main.js
+++ src/shopdesk/static/js/main.js
@@ -1,14 +1,7 @@
 jQuery(document).ready(function() {
-            jQuery(".side-links .hide").click(function() {
-                        var element = jQuery(this);
-                        var sideLinks = element.parents(".side-links");
-                        sideLinks.triggerHandler("hide");
-                    });
-
             jQuery(".side-links .swindow").click(function() {
-                        alert("ola");
+                        alert("Hello World");
                     });
-
 
             jQuery(".side-links .side").click(function() {
                         jQuery(".side-panel").triggerHandler("toggle");
