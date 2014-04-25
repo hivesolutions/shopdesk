de57fa040f1dc58fd0ee2c772b6a0a48ac4a4f48
joamag <joamag@gmail.com>
2014-04-25 14:05:19 +0100

    new admin support

--- src/shopdesk/main.py
+++ src/shopdesk/main.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import appier
+import appier_extras
 
 class ShopdeskApp(appier.WebApp):
 
     def __init__(self):
         appier.WebApp.__init__(
             self,
-            name = "shopdesk"
+            name = "shopdesk",
+            parts = (
+                appier_extras.AdminPart,
+            )
         )
 
 if __name__ == "__main__":
