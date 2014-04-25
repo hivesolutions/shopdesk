50c8734eccf28b2160ed3429510097b8ff52b93c
joamag <joamag@gmail.com>
2014-04-25 18:57:22 +0100

    small enumeration fix

--- src/shopdesk/models/order.py
+++ src/shopdesk/models/order.py
@@ -14,6 +14,7 @@ class Order(appier_extras.admin.Base):
 
     PAYMENT_S = {
         PENDING : "pending",
+        ISSUED : "issued",
         PAID : "paid",
         REFUNDED : "refunded",
         OTHER : "other"
