b5beeeeba151b2a1b5bf651e546770d227e7477b
joamag <joamag@gmail.com>
2014-04-25 18:45:49 +0100

    initial scheduler structure

--- src/shopdesk/scheduler.py
+++ src/shopdesk/scheduler.py
@@ -4,7 +4,12 @@
 import time
 import threading
 
-LOOP_TIMEOUT = 15.0
+import appier
+import shopify
+import easypay
+import shopdesk
+
+LOOP_TIMEOUT = 60.0
 
 class Scheduler(threading.Thread):
     """
@@ -15,13 +20,14 @@ class Scheduler(threading.Thread):
     new task may be added to it through a queue system.
     """
 
-    def __init__(self, api):
+    def __init__(self, owner):
         threading.Thread.__init__(self)
-        self.api = api
+        self.owner = owner
         self.daemon = True
 
     def run(self):
         self.running  = True
+        self.load()
         while self.running:
             self.tick()
             time.sleep(LOOP_TIMEOUT)
@@ -30,4 +36,61 @@ class Scheduler(threading.Thread):
         self.running = False
 
     def tick(self):
-        pass
+        self.check_orders()
+        self.issue_references()
+
+    def load(self):
+        self.load_shopify()
+        self.load_easypay()
+
+    def load_shopify(self):
+        self.shopify = shopify.Api(
+            api_key = appier.conf("SHOPIFY_API_KEY"),
+            password = appier.conf("SHOPIFY_PASSWORD"),
+            secret = appier.conf("SHOPIFY_SECRET"),
+            store_url = appier.conf("SHOPIFY_STORE")
+        )
+
+    def load_easypay(self):
+        self.easypay = easypay.ShelveApi(
+            username = appier.conf("EASYPAY_USERNAME"),
+            password = appier.conf("EASYPAY_PASSWORD"),
+            cin = appier.conf("EASYPAY_CIN"),
+            entity = appier.conf("EASYPAY_ENTITY")
+        )
+        self.easypay.bind("paid", self.on_paid)
+
+    def check_orders(self):
+        self.owner.logger.debug("Checking shopify orders ...")
+        orders = self.shopify.list_orders(limit = 30)
+        new_orders = []
+        for order in orders:
+            _order = shopdesk.Order.get(s_id = order["id"], raise_e = False)
+            if _order: continue
+            new_orders.append(order)
+        self.owner.logger.debug("Found '%d' new shopify orders", len(new_orders))
+        for order in new_orders:
+            _order = shopdesk.Order(
+                s_id = order["id"],
+                s_name = order["name"],
+                s_total_price = order["total_price"],
+                s_gateway = order["gateway"],
+                s_status = order["financial_status"]
+            )
+            _order.save()
+
+    def issue_references(self):
+        orders = shopdesk.Order.find(payment = shopdesk.Order.PENDING)
+        self.owner.logger.debug("Issuing references for '%d' orders ...")
+        for order in orders:
+            amount = float(order.s_total_price)
+            reference = self.easypay.generate_mb(amount)
+            order.reference = reference
+            order.payment = shopdesk.Order.ISSUED
+            order.save()
+
+    def on_paid(self, reference, details):
+        identifier = reference["identifier"]
+        order = shopdesk.Order.get(reference = identifier, raise_e = False)
+        self.owner.logger.debug("Received payment for order '%s'" % order.name)
+        #@todo should trigger an event fo email notification
