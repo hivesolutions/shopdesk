6e22ee5bbd484bcf3898da1b7f19d8d8a1a0b792
joamag <joamag@gmail.com>
2014-04-26 13:02:26 +0100

    new order based interactions

--- src/shopdesk/models/order.py
+++ src/shopdesk/models/order.py
@@ -91,3 +91,24 @@ class Order(appier_extras.admin.Base):
         if self.s_status == "pending": self.payment = Order.PENDING; return
         if self.s_status == "refunded": self.payment = Order.REFUNDED; return
         self.payment = Order.PAID
+
+    def issue_reference_s(self, easypay):
+        amount = float(self.s_total_price)
+        reference = easypay.generate_mb(amount)
+        self.reference = reference["identifier"]
+        self.payment = Order.ISSUED
+        self.save()
+        self.owner.logger.debug("Issue reference for order '%s'" % self.s_name)
+
+    def pay_s(self, shopify):
+        shopify.pay_order(self.s_id)
+        self.payment = Order.PAID
+        self.save()
+        self.owner.logger.debug("Received payment for order '%s'" % self.s_name)
+
+    def cancel_s(self, easypay, shopify):
+        easypay.cancel_mb(self.reference)
+        shopify.cancel_order(self.s_id)
+        self.payment = Order.CANCELED
+        self.save()
+        self.owner.logger.debug("Canceled and reversed order '%s'" % self.s_name)
