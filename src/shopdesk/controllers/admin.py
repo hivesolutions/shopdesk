3d4f565c298641df8e6ef3a96c601ffc03821db7
joamag <joamag@gmail.com>
2014-04-30 21:11:03 +0100

    new test email

--- src/shopdesk/controllers/admin.py
+++ src/shopdesk/controllers/admin.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import appier
+import appier_extras
 
 class AdminController(appier.Controller):
 
@@ -12,3 +13,18 @@ class AdminController(appier.Controller):
     @appier.ensure("admin")
     def easypay(self):
         return self.scheduler.easypay.diagnostics()
+
+    @appier.route("/admin/email.json", "GET")
+    @appier.ensure("admin")
+    def email_test(self):
+        email = self.field("email", None)
+        if not email: raise appier.OperationalError(
+            messsage = "No email defined"
+        )
+        base = appier_extras.admin.Base()
+        base.send_email(
+            "email/test.html.tpl",
+            receivers = [email],
+            subject = self.to_locale("Shopdesk test email")
+        )
+        return dict(email = email)
---
+++ src/shopdesk/templates/email/test.html.tpl
@@ -0,0 +1,7 @@
+{% extends "email/layout.html.tpl" %}
+{% block title %}Test{% endblock %}
+{% block content %}
+    <p>
+        This is a simple test mail for the Shopdesk infra-structure
+    </p>
+{% endblock %}
