8da387e2dcf9b4718a23f3a7081eed951b43531f
joamag <joamag@gmail.com>
2014-05-07 15:59:59 +0100

    new list names

--- src/shopdesk/models/order.py
+++ src/shopdesk/models/order.py
@@ -116,6 +116,10 @@ class Order(appier_extras.admin.Base):
         ]
 
     @classmethod
+    def list_names(cls):
+        return ["s_name", "s_total_price", "s_gateway", "s_billing_name", "s_status"]
+
+    @classmethod
     def from_shopify(cls, order):
         return cls(
             s_id = order["id"],
