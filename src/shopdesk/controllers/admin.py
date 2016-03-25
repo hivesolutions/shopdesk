6c63d38f1081ff62cf32e6d25ee296e3ebcb4fdc
joamag <joamag@gmail.com>
2016-03-25 13:02:46 +0000

    small fixes

--- src/shopdesk/controllers/admin.py
+++ src/shopdesk/controllers/admin.py
@@ -15,12 +15,14 @@ class AdminController(appier.Controller):
 
     @appier.route("/admin/email.json", "GET")
     @appier.ensure("admin")
-    def email_test(self):
+    def email_test(self, owner = None):
+        owner = owner or appier.get_app()
         email = self.field("email", None)
         if not email: raise appier.OperationalError(
             message = "No email defined"
         )
         appier_extras.admin.Base.send_email_g(
+            owner,
             "email/test.html.tpl",
             receivers = [email],
             subject = self.to_locale("Shopdesk test email")
