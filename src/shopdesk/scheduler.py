fa31c03d34a4fdc08672b79cf55613fb30d45b7b
joamag <joamag@gmail.com>
2016-11-02 19:54:02 +0000

    small code fix

--- src/shopdesk/scheduler.py
+++ src/shopdesk/scheduler.py
@@ -91,7 +91,8 @@ class Scheduler(appier.Scheduler):
         self.logger.debug("Found '%d' new shopify orders" % len(new_orders))
         for order in new_orders:
             order_id = order["id"]
-            self.logger.debug("Importing shopify order '%d' ..." % order_id)
+            order_name = order["name"]
+            self.logger.debug("Importing shopify order '%s' ..." % order_name)
             transactions = self.shopify.transactions_order(order_id)
             _order = shopdesk.Order.from_shopify(order, transactions = transactions)
             _order.save()
